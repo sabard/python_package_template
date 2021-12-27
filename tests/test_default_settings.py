bb1521440874e548d1bc27b91392ee83d6a4d418
sabard <s@bardasgupta.com>
2021-12-27 14:02:53 -0500

    pytest working

--- tests/test_default_settings.py
+++ tests/test_default_settings.py
@@ -8,30 +8,6 @@ from plumbum import local
 
 logger = logging.getLogger(__name__)
 
-
-def get_gh_workflows_schema(cloned_template: Path):
-    """This function loads the given GH Workflows schema available"""
-    schema_file = Path(
-        cloned_template,
-        "tests",
-        "schemastore",
-        "src",
-        "schemas",
-        "json",
-        "github-workflow.json",
-    )
-    with schema_file.open("r") as file:
-        data = file.read()
-    schema = yaml.safe_load(data)
-    return schema
-
-
-def validate_schema(yaml_data, cloned_template: Path):
-    """Validate GH Workflows yaml based on expected schema."""
-    expected_schema = get_gh_workflows_schema(cloned_template)
-    jsonschema.validate(instance=yaml_data, schema=expected_schema)
-
-
 def test_default_settings(tmp_path: Path, cloned_template: Path):
     """Test that a template can be rendered from zero."""
     with local.cwd(cloned_template):
@@ -41,30 +17,15 @@ def test_default_settings(tmp_path: Path, cloned_template: Path):
             vcs_ref="test",
             force=True,
             data={
-                "project_name": "docker-test",
+                "project_name": "project_name",
                 "project_owner": "Test",
-                "dockerhub_image": "test/test",
             },
         )
     with local.cwd(tmp_path):
         # Check that files exist
-        assert Path(".github", "workflows", "ci.yml").exists()
-        assert Path(".copier-answers.image-template.yml").exists()
+        assert Path(".copier-answers.yml").exists()
         # Tests are included by default
-        assert Path("pytest.ini").exists()
-        assert Path("pyproject.toml").exists()
         assert Path("tests/conftest.py").exists()
-        # Validate CI config
-        with Path(".github", "workflows", "ci.yml").open("r") as f:
-            content = f.read()
-            yaml_data = yaml.safe_load(content)
-            # Ensure project data propagated
-            assert (
-                yaml_data["jobs"]["build-push"]["env"]["DOCKERHUB_IMAGE_NAME"]
-                == "test/test"
-            )
-            # Validate according to Github Actions expected syntax
-            validate_schema(yaml_data, cloned_template)
 
 
 def test_no_pytest_settings(tmp_path: Path, cloned_template: Path):
@@ -76,23 +37,12 @@ def test_no_pytest_settings(tmp_path: Path, cloned_template: Path):
             vcs_ref="test",
             force=True,
             data={
-                "project_name": "docker-test",
-                "project_owner": "Test",
-                "dockerhub_image": "test/test",
+                "project_name": "project_name",
                 "pytest": False,
             },
         )
     with local.cwd(tmp_path):
         # Check that files exist
-        assert Path(".github", "workflows", "ci.yml").exists()
-        assert Path(".copier-answers.image-template.yml").exists()
+        assert Path(".copier-answers.yml").exists()
         # Tests shouldn't exist
-        assert not Path("pytest.ini").exists()
-        assert not Path("pyproject.toml").exists()
         assert not Path("tests/conftest.py").exists()
-        # Validate CI config
-        with Path(".github", "workflows", "ci.yml").open("r") as f:
-            content = f.read()
-            yaml_data = yaml.safe_load(content)
-            # Validate according to Github Actions expected syntax
-            validate_schema(yaml_data, cloned_template)
